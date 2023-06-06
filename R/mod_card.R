4240370c3020477ca19f7211bdc5269e962b4208
keatonwilson <keatonwilson@me.com>
2023-06-06 14:57:52 -0400

    Added dummy layout and lorem ipsum for About page. Outlines module usage, as well as some basic themeing and the use of bslib for layout.

---
+++ R/mod_card.R
@@ -0,0 +1,19 @@
+#TODO Improve documentation
+
+card_ui <- function(id, 
+                  header, 
+                  content) {
+  
+  bslib::card(
+  bslib::card_header(header),
+  content
+
+)
+  
+}
+
+card_server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    # We don't need anything in the server for this module
+  })
+}
