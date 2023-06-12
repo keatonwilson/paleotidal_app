f0ca0bb9a314501a99f810aefc2eac4bcd8322b8
Jessica Guo <jessicaguo@email.arizona.edu>
2023-06-11 17:40:33 -0700

    Testing module vs. old fashioned way

--- ui.R
+++ ui.R
@@ -4,7 +4,15 @@ bslib::page_navbar(theme = bslib::bs_theme(bootswatch = "yeti"),
                    title = "Paleotidal Visualization", 
                    bslib::nav_panel("About", 
                                     about_tab_ui("about_tab_content")), 
-                   bslib::nav_panel("Explore Data Visualizations"), 
+                   bslib::nav_panel("Explore Data Visualizations",
+                                    # map_ui("map") # module version, still not working
+                                    layout_column_wrap(
+                                      width = 1/2, 
+                                      card(full_screen = TRUE,
+                                           card_header("Map"),
+                                           plotOutput("map"))
+                                    )
+                   ), 
                    bslib::nav_spacer(), 
                    bslib::nav_menu("Supporting Entities", 
                                    align = "right", 
