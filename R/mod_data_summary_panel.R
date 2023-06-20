871e31e9a948748e06e9c859e86fad10adb18e50
keatonwilson <keatonwilson@me.com>
2023-06-19 17:26:12 -0700

    buildout module for data summary panel

---
+++ R/mod_data_summary_panel.R
@@ -0,0 +1,61 @@
+data_summary_ui <- function(id){
+  ns <- NS(id)
+
+    bslib::layout_columns(
+      col_widths = c(4, 4, 4),
+      bslib::value_box(
+        "Year",
+        showcase = bsicons::bs_icon("hourglass-split"),
+        value = shiny::textOutput(ns("year_bp"))
+      ),
+      bslib::value_box(
+        "Dataset",
+        showcase = bsicons::bs_icon("database"),
+        value = shiny::textOutput(ns("dataset"))
+      ),
+      bslib::value_box(
+        "Legend",
+        showcase = bsicons::bs_icon("compass-fill"),
+        value = shiny::htmlOutput(ns("legend"))
+      )
+    )
+  
+}
+
+data_summary_server <- function(id, 
+                                year, 
+                                dataset, 
+                                legend) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+      
+
+# Year --------------------------------------------------------------------
+
+      year_to_print = prettyNum(year,
+                                big.mark=",",
+                                scientific=FALSE)
+      # rendering
+      output$year_bp = shiny::renderText({
+        glue::glue("{year_to_print} BP")
+      })
+
+# Dataset -----------------------------------------------------------------
+
+      output$dataset = shiny::renderText({
+        dataset
+      }) 
+      
+
+# Legend ------------------------------------------------------------------
+
+      output$legend = shiny::renderUI({
+        # might need more complex html here - hence the different output
+        shiny::HTML(legend)
+      })      
+      
+      
+    }
+  )
+}
