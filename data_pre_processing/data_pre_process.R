d28fc080a419ad5d35b285c718e5b4d278a83b5c
keatonwilson <keatonwilson@me.com>
2023-06-25 18:00:12 -0700

    Function for combining all data from a single directory into a dataframe with year and datatype as columns (e.g. tidy)

--- data_pre_processing/data_pre_process.R
+++ data_pre_processing/data_pre_process.R
@@ -32,3 +32,39 @@ crs(r_new) = "+proj=longlat +datum=WGS84"
 leaflet() |> 
   addTiles() |> 
   addRasterImage(r_new, opacity = 0.8)
+
+
+
+# Combining All Years -----------------------------------------------------
+
+combine_all_years = function(data_dir, 
+                             datatype
+                             ) {
+  # files to read in
+  files_to_read = list.files(data_dir, full.names = TRUE)
+  
+  purrr::map(files_to_read, function(file) {
+    # extracting year
+    year = stringr::str_extract(file, "(\\d{2})(?=\\.[^.]+$)") |> 
+      as.numeric()
+    
+    # reading file and appending 
+    data = readr::read_tsv(file, 
+                           col_names = c("x", "y", "value"), 
+                           show_col_types = FALSE) |> 
+      dplyr::mutate(year = year, 
+                    datatype = datatype)
+    
+    # return
+    return(data)
+    
+  }) |> 
+    dplyr::bind_rows()
+
+}
+
+amp_test = combine_all_years("./data/raw_lat_lon/ampM2/", 
+                  "amplitude")
+
+
+
