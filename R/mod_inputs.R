68462c5e9f5e8773cae56a45700c57c9c899502e
Jessica Guo <jessicaguo@email.arizona.edu>
2023-06-27 17:07:31 -0700

    Add reactive UI elements, or try to

--- R/mod_inputs.R
+++ R/mod_inputs.R
@@ -2,7 +2,9 @@
 
 input_ui <- function(id) {
   tagList(
+    shinyjs::useShinyjs(),
     bslib::navset_card_tab(
+      id = "tabs",
       full_screen = TRUE,
       title = "User inputs",
       bslib::nav_panel("General",
@@ -23,11 +25,6 @@ input_ui <- function(id) {
                               choices = 0:21,
                               selected = 0,
                               width = '70%'))),
-                sliderInput(NS(id, "year"), "Display kyears before present:", 
-                            min = 0, 
-                            max = 21,
-                            value = 21, 
-                            width = "100%"),
                 shinyWidgets::sliderTextInput(NS(id, "animateyear"), "Animate through time:",
                             choices = 21:0,
                             selected = 21, 
@@ -36,29 +33,32 @@ input_ui <- function(id) {
                             animate = animationOptions(interval = 500,
                                                        loop = FALSE))),
       bslib::nav_panel("Custom",
-                       bslib::card(bslib::card_title("Stratification"),
-                                   bslib::layout_column_wrap(
-                                     width = 1/2,
-                                     bslib::card(bslib::card_title("Boundary values"),
-                                                 shinyWidgets::numericRangeInput(NS(id, "boundaryrange"), "Set min and max:",
-                                                                                 value = c(1.9, 2.9),
-                                                                                 min = 0, max = 12,
-                                                                                 step = 0.1),
-                                                 checkboxInput(NS(id, "contrast"), "Show contrast",
-                                                               value = FALSE)),
-                                     bslib::card(bslib::card_title("Front values"),
-                                                 numericInput(NS(id, "frontvalue"), "Set front value:",
-                                                              value = 2.1,
-                                                              min = 1.9, max = 2.9, # dynamic, updated with range from boundary values
-                                                              step = 0.1),
-                                                 numericInput(NS(id, "radius"), "Set radius of front:",
-                                                              value = 0.08,
-                                                              min = 0.01, max = 1,
-                                                              step = 0.01),
-                                                 checkboxInput(NS(id, "front"), "Show front",
-                                                               value = TRUE),
-                                                 checkboxInput(NS(id, "gradient"), "Show gradient",
-                                                               value = FALSE)))),
+                       shinyjs::hidden(
+                         div(id = "strat_card",
+                             bslib::card(bslib::card_title("Stratification"),
+                                         bslib::layout_column_wrap(
+                                           width = 1/2,
+                                           bslib::card(bslib::card_title("Boundary values"),
+                                                       shinyWidgets::numericRangeInput(NS(id, "boundaryrange"), "Set min and max:",
+                                                                                       value = c(1.9, 2.9),
+                                                                                       min = 0, max = 12,
+                                                                                       step = 0.1),
+                                                       checkboxInput(NS(id, "contrast"), "Show contrast",
+                                                                     value = FALSE)),
+                                           bslib::card(bslib::card_title("Front values"),
+                                                       numericInput(NS(id, "frontvalue"), "Set front value:",
+                                                                    value = 2.1,
+                                                                    min = 1.9, max = 2.9, # dynamic, updated with range from boundary values
+                                                                    step = 0.1),
+                                                       numericInput(NS(id, "radius"), "Set radius of front:",
+                                                                    value = 0.08,
+                                                                    min = 0.01, max = 1,
+                                                                    step = 0.01),
+                                                       checkboxInput(NS(id, "front"), "Show front",
+                                                                     value = TRUE),
+                                                       checkboxInput(NS(id, "gradient"), "Show gradient",
+                                                                     value = FALSE))))
+                         )),
                        bslib::card(bslib::card_title("Peak Stress Vectors"),
                                    bslib::layout_column_wrap(
                                      width = 1/2,
@@ -86,6 +86,28 @@ input_ui <- function(id) {
 
 input_server <- function(id) {
   moduleServer(id, function(input, output, session){
+    
+    # Grays out coastyear UI if ghostcoast == FALSE
+    observeEvent(input$ghostcoast, {
+      shinyjs::toggleState(id = "coastyear", 
+                           condition = input$ghostcoast)
+    })
+    
+    # Attempt to remove Custom tab for two Tidal dataproducts
+    observeEvent(input$dataproduct, {
+      if(input$dataproduct %in% c("Tidal Amplitude", "Tidal Current")) {
+        bslib::nav_remove(id = "tabs", target = "Custom")
+      }
+    })
+    
+    # Set Custom card depending on data product
+    observeEvent(input$dataproduct, {
+      # is_strat <- ifelse(input$dataproduct == "Stratification", TRUE, FALSE)
+      if(input$dataproduct == "Stratification") {
+        shinyjs::show(id = "strat_card", 
+                      anim = TRUE)
+      }
+    })
     
     # General inputs (applicable to all datasets)
     reactive({
