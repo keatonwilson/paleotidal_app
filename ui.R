4b7ea8c92e024d9cd480a657b7265d0d4231cee5
Jessica Guo <jessicaguo@email.arizona.edu>
2023-07-01 10:00:05 -0700

    Setting theme lux and font Garamond

--- ui.R
+++ ui.R
@@ -1,8 +1,12 @@
 
 # Define UI for application that draws a histogram
-bslib::page_navbar(theme = bslib::bs_theme(bootswatch = "solar",
-                                           # base_font = bslib::font_google("Cormorant Garamond"),
-                                           base_font = bslib::font_google("Yanone Kaffeesatz")),
+bslib::page_navbar(
+  # theme = bslib::bs_theme(bootswatch = "solar",
+  # base_font = bslib::font_google("Cormorant Garamond"),
+  # base_font = bslib::font_google("Yanone Kaffeesatz"),
+  # base_font = bslib::font_google("Playfair Display")),
+                   theme = bslib::bs_theme(bootswatch = "lux",
+                                           base_font = bslib::font_google("Cormorant Garamond")),
                    title = "Paleotidal Visualization", 
                    bslib::nav_panel("About", 
                                     about_tab_ui("about_tab_content")), 
