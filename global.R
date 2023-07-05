fd876f3da5b6ac9efa17df39d0497211292730da
keatonwilson <keatonwilson@me.com>
2023-07-04 21:09:11 -0700

    feather read in

--- global.R
+++ global.R
@@ -1,9 +1,16 @@
 
 # Data load ---------------------------------------------------------------
 
-
+# rasters
 amp_raster = readr::read_rds("./data/processed_data/amp_raster.rds")
 bss_raster = readr::read_rds("./data/processed_data/bss_raster.rds")
 mask_water_raster = readr::read_rds("./data/processed_data/mask_water_raster.rds")
 rsl_raster = readr::read_rds("./data/processed_data/rsl_raster.rds")
 water_depth_raster = readr::read_rds("./data/processed_data/water_depth_raster.rds")
+
+# tidy data
+amp_data = arrow::read_feather("./data/processed_data/amp_data.feather")
+bss = arrow::read_feather("./data/processed_data/bss.feather")
+rsl = arrow::read_feather("./data/processed_data/rsl.feather")
+mask_water = arrow::read_feather("./data/processed_data/mask_water.feather")
+water_depth = arrow::read_feather("./data/processed_data/water_depth.feather")
