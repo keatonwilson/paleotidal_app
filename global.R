f796f7de5137f1bb3a7835f1b4b4a28ada30ab5c
keatonwilson <keatonwilson@me.com>
2023-07-20 12:08:56 -0700

    Ice layer

--- global.R
+++ global.R
@@ -15,6 +15,11 @@ rsl = arrow::read_feather("./data/processed_data/rsl.feather")
 mask_water = arrow::read_feather("./data/processed_data/mask_water.feather")
 water_depth = arrow::read_feather("./data/processed_data/water_depth.feather")
 
-# coastline shape files
+# shapefiles
 shape_1 = sf::st_read("./data/raw_shape/coastline/GSHHS_l_L1.shp")
+
+strat_contours = list.files("./data/raw_shape/strat/", full.names = TRUE)[stringr::str_detect(list.files("./data/raw_shape/strat/"), ".shp")] |>
+  purrr::map(function(shapefile) {
+  sf::st_read(shapefile)
+})
 
