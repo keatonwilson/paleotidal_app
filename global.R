8e93b16eaabde6e8826734c69b290c5c9d2ee0e0
keatonwilson <keatonwilson@me.com>
2023-07-23 17:32:41 -0700

    starting to buildout deployment logic switch in global.R

--- global.R
+++ global.R
@@ -1,30 +1,41 @@
+# Set local or deloyment mode --------------------------------------------
+is_deployment = FALSE
+
 
 # Data load ---------------------------------------------------------------
+if (is_deployment == FALSE) {
+  # rasters
+  amp_raster = readr::read_rds("./data/processed_data/amp_raster.rds")
+  bss_raster = readr::read_rds("./data/processed_data/bss_raster.rds")
+  mask_water_raster = readr::read_rds("./data/processed_data/mask_water_raster.rds")
+  rsl_raster = readr::read_rds("./data/processed_data/rsl_raster.rds")
+  strat_raster = readr::read_rds("./data/processed_data/strat_raster.rds")
+  vel_raster = readr::read_rds("./data/processed_data/vel_raster.rds")
+  water_depth_raster = readr::read_rds("./data/processed_data/water_depth_raster.rds")
+  ice_raster = readr::read_rds("./data/processed_data/ice_raster.rds")
+  
+  # tidy data
+  amp_data = arrow::read_feather("./data/processed_data/amp_data.feather")
+  bss = arrow::read_feather("./data/processed_data/bss.feather")
+  mask_water = arrow::read_feather("./data/processed_data/mask_water.feather")
+  rsl = arrow::read_feather("./data/processed_data/rsl.feather")
+  strat = arrow::read_feather("./data/processed_data/strat.feather")
+  vel = arrow::read_feather("./data/processed_data/vel.feather")
+  water_depth = arrow::read_feather("./data/processed_data/water_depth.feather")
+  
+  # shapefiles
+  shape_1 = sf::st_read("./data/raw_shape/coastline/GSHHS_l_L1.shp")
+  
+  strat_contours = list.files("./data/raw_shape/strat/", full.names = TRUE)[stringr::str_detect(list.files("./data/raw_shape/strat/"), ".shp")] |>
+    purrr::map(function(shapefile) {
+    sf::st_read(shapefile)
+  })
 
-# rasters
-amp_raster = readr::read_rds("./data/processed_data/amp_raster.rds")
-bss_raster = readr::read_rds("./data/processed_data/bss_raster.rds")
-mask_water_raster = readr::read_rds("./data/processed_data/mask_water_raster.rds")
-rsl_raster = readr::read_rds("./data/processed_data/rsl_raster.rds")
-strat_raster = readr::read_rds("./data/processed_data/strat_raster.rds")
-vel_raster = readr::read_rds("./data/processed_data/vel_raster.rds")
-water_depth_raster = readr::read_rds("./data/processed_data/water_depth_raster.rds")
-ice_raster = readr::read_rds("./data/processed_data/ice_raster.rds")
-
-# tidy data
-amp_data = arrow::read_feather("./data/processed_data/amp_data.feather")
-bss = arrow::read_feather("./data/processed_data/bss.feather")
-mask_water = arrow::read_feather("./data/processed_data/mask_water.feather")
-rsl = arrow::read_feather("./data/processed_data/rsl.feather")
-strat = arrow::read_feather("./data/processed_data/strat.feather")
-vel = arrow::read_feather("./data/processed_data/vel.feather")
-water_depth = arrow::read_feather("./data/processed_data/water_depth.feather")
-
-# shapefiles
-shape_1 = sf::st_read("./data/raw_shape/coastline/GSHHS_l_L1.shp")
-
-strat_contours = list.files("./data/raw_shape/strat/", full.names = TRUE)[stringr::str_detect(list.files("./data/raw_shape/strat/"), ".shp")] |>
-  purrr::map(function(shapefile) {
-  sf::st_read(shapefile)
-})
-
+} else {
+  bucketfiles = aws.s3::get_bucket('paleotidal-data-storage')
+  amp_raster = aws.s3::s3readRDS("processed_data/amp_raster.rds", 
+                                 bucket = "paleotidal-data-storage")
+  amp_data = aws.s3::get_object("processed_data/amp_data.feather", 
+                                bucket = "paleotidal-data-storage") |> 
+    arrow::read_feather()
+}
