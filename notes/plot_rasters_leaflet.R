482924caddfe0f8be64fbfbbf78a906d1601c9d4
Jessica Guo <jessicaguo@email.arizona.edu>
2023-07-24 17:38:59 -0700

    Test colorFactor for leaflet

--- notes/plot_rasters_leaflet.R
+++ notes/plot_rasters_leaflet.R
@@ -5,12 +5,12 @@ library(tidyverse)
 library(leaflet)
 
 # Sandbox to make leaflet plots locally before connecting to app
-
+shape_1 = sf::st_read("./data/raw_shape/coastline/GSHHS_l_L1.shp")
 ice_raster = readr::read_rds("./data/processed_data/ice_raster.rds")
 
 # Start with veolocity
 vel_raster = readr::read_rds("./data/processed_data/vel_raster.rds")
-shape_1 = sf::st_read("./data/raw_shape/coastline/GSHHS_l_L1.shp")
+
 
 leaflet() |> 
   leaflet::setView(lng = -4, lat = 56, zoom = 4.5) |> 
@@ -24,16 +24,17 @@ leaflet() |>
 # Test strat
 strat_raster = readr::read_rds("./data/processed_data/strat_raster.rds")
 
-color_vec <- rev(RColorBrewer::brewer.pal(3, "GnBu"))
-RColorBrewer::display.brewer.pal(3, "GnBu")
+# color_vec <- rev(RColorBrewer::brewer.pal(3, "GnBu"))
+# RColorBrewer::display.brewer.pal(3, "GnBu")
 
 pal <- colorFactor(palette = "GnBu",
-                   domain = values(strat_raster[[1]]),
-                   na.color = "black")
+                   domain = 1:3,
+                   na.color = "gray20", 
+                   reverse = TRUE)
 
 leaflet() |> 
-  addTiles() |> 
-  addRasterImage(strat_raster[[5]], opacity = 0.8, colors = pal) |> 
+  addRasterImage(strat_raster[[21]], opacity = 0.8, colors = pal) |> 
   addPolygons(data = shape_1, color = "black", weight = 1,
-              opacity = 1, fillOpacity = 0)
+              opacity = 1, fillOpacity = 0) |>
+  addRasterImage(ice_raster[[21]], colors = "aliceblue")
 
