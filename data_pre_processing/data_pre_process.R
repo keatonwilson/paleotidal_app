edc8c9d73a61b27b1c6d7b43a7e477f5234bc907
keatonwilson <keatonwilson@me.com>
2023-07-26 17:22:13 -0700

    Update preprocessing script

--- data_pre_processing/data_pre_process.R
+++ data_pre_processing/data_pre_process.R
@@ -14,32 +14,32 @@ library(leaflet)
 
 # Testing Sandbox ---------------------------------------------------------
 
-test = read_tsv("./data/raw_lat_lon/ampM2/amp_M2_00.ascii", 
-                col_names = c("x", "y", "value"))
-test = read_tsv("./data/raw_lat_lon/stratification/log10_strat_10.ascii", 
-                col_names = c("x", "y", "value")) %>%
-  mutate(value = if_else(is.nan(value), NA, value),
-         value = if_else(is.infinite(value), NA, value))
-test = read_tsv("./data/raw_lat_lon/stratification/strat_00.ascii", 
-                col_names = c("x", "y", "value"))
-test = read_tsv("./data/raw_lat_lon/vel/velM2_00.ascii", 
-                col_names = c("x", "y", "value"))
+# test = read_tsv("./data/raw_lat_lon/ampM2/amp_M2_00.ascii", 
+#                 col_names = c("x", "y", "value"))
+# test = read_tsv("./data/raw_lat_lon/stratification/log10_strat_10.ascii", 
+#                 col_names = c("x", "y", "value")) %>%
+#   mutate(value = if_else(is.nan(value), NA, value),
+#          value = if_else(is.infinite(value), NA, value))
+# test = read_tsv("./data/raw_lat_lon/stratification/strat_00.ascii", 
+#                 col_names = c("x", "y", "value"))
+# test = read_tsv("./data/raw_lat_lon/vel/velM2_00.ascii", 
+#                 col_names = c("x", "y", "value"))
 
 # getting dims for raster transformation
-ncol = length(unique(test$x))
-nrow = length(unique(test$y))
-
-extent = extent(test[,(1:2)])
-r = raster(extent, ncol = ncol, nrow = nrow)
-
-# rasterize
-r_new = rasterize(test[,1:2], r, test[,3], fun=mean)
-crs(r_new) = "+proj=longlat +datum=WGS84"
-
-# testing on map
-leaflet() |> 
-  addTiles() |> 
-  addRasterImage(r_new, opacity = 0.8)
+# ncol = length(unique(test$x))
+# nrow = length(unique(test$y))
+# 
+# extent = extent(test[,(1:2)])
+# r = raster(extent, ncol = ncol, nrow = nrow)
+# 
+# # rasterize
+# r_new = rasterize(test[,1:2], r, test[,3], fun=mean)
+# crs(r_new) = "+proj=longlat +datum=WGS84"
+# 
+# # testing on map
+# leaflet() |> 
+#   addTiles() |> 
+#   addRasterImage(r_new, opacity = 0.8)
 
 
 
